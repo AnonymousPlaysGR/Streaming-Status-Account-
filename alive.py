5815f0de802f5ebd0d6dbb1cc44a49b7c28a7a70
AnonymousPlaysGR <142651157+AnonymousPlaysGR@users.noreply.github.com>
2023-10-19 02:37:04 +0300

    alive.py

--- alive.py
+++ alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def main():
-  return "join our discord server here https://discord.gg/7rjU4SfZwy"
+  return "join our discord server here https://discord.com/invite/BUbm3Qk5w4"
 
 def run():
   app.run(host="0.0.0.0", port=8080)
